cb1fcecd477f9e176682b33b65fa1fe60a38ee50
Nithyasrisomu19 <nnithyasri66@gmail.com>
2024-08-22 21:26:51 +0530

    Add files via upload

---
+++ users.js
@@ -0,0 +1,8 @@
+const Users=[
+    {id:"1", Name:"Nithi",Email:"Nithi@gmail.com",MobileNumber:"9003379241",Age:23,Course:"React"},
+    {id:"2", Name:"Suriya",Email:"suriya@gmail.com",MobileNumber:"9822342001",Age:21,Course:"Angular"},
+    {id:"3", Name:"Priya",Email:"priyahari@gmail.com",MobileNumber:"9834532954",Age:20,Course:"Django"},
+    {id:"4", Name:"Harshitha",Email:"harshitha@gmail.com",MobileNumber:"9456238968",Age:19,Course:"Flask"}
+];      //Default records
+
+export default Users;
